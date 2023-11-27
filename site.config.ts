5e8f34ff38d96cc91531ec9c6101affe6565ec4b
島谷　悠 <shimataniyu1209@icloud.com>
2023-11-27 22:32:48 +0900

    環境変数の変更

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'cc2ef7c88ec745b0ab149915f6a78fcb',
+  rootNotionPageId: process.env.NOTION_ID,
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
